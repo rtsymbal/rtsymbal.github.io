9dd743889196780fc46bb46b08f06d84af52337f
Roman Tsymbal <roman.tsymbal@gmail.com>
2017-10-18 21:38:51 +0200

    main menu creation

--- pretty-girl/app/js/common.js
+++ pretty-girl/app/js/common.js
@@ -1,5 +1,20 @@
 $(function() {
 
-	// Custom JS
+	$('#my-menu').mmenu({
+		extensions: [ 'theme-black', 'fx-menu-slide', 'pagedim-black' ],
+		navbar: {
+			title: '<img src="img/logo.svg">'
+		},
+		offCanvas: {
+			position: 'right'
+         }
+	});
 
+	var api = $('#my-menu').data('mmenu');
+	
+	api.bind('opened', function() {
+		$('.hamburger').addClass('is-active');
+	}).bind('closed', function() {
+		$('.hamburger').removeClass('is-active');
+	});
 });
