5012f33871d34db4edc1201d7d25b4a38c9d5d56
Roman Tsymbal <roman.tsymbal@gmail.com>
2017-10-19 20:39:19 +0200

    gamburger button animation

--- pretty-girl/app/js/common.js
+++ pretty-girl/app/js/common.js
@@ -12,9 +12,30 @@ $(function() {
 
 	var api = $('#my-menu').data('mmenu');
 	
-	api.bind('opened', function() {
+	api.bind('open:finish', function() {
 		$('.hamburger').addClass('is-active');
-	}).bind('closed', function() {
+	}).bind('close:finish', function() {
 		$('.hamburger').removeClass('is-active');
 	});
 });
+
+// var $menu = $("#my-menu").mmenu({
+//    //   options
+// });
+// var $icon = $("#my-icon");
+// var API = $menu.data( "mmenu" );
+
+// $icon.on( "click", function() {
+//    API.open();
+// });
+
+// API.bind( "open:finish", function() {
+//    setTimeout(function() {
+//       $icon.addClass( "is-active" );
+//    }, 100);
+// });
+// API.bind( "close:finish", function() {
+//    setTimeout(function() {
+//       $icon.removeClass( "is-active" );
+//    }, 100);
+// });
