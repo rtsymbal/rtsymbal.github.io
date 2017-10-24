a1222802dba540ac18caf5addf91a8250808ed51
Roman Tsymbal <roman.tsymbal@gmail.com>
2017-10-24 21:37:37 +0200

    carousel dev ending

--- pretty-girl/app/js/common.js
+++ pretty-girl/app/js/common.js
@@ -23,12 +23,20 @@ $(function() {
 			}, 100);
 	});
 
+
+	$('.carousel-services').on('initialized.owl.carousel', function() {
+		setTimeout(function() {
+			carouselService()
+		},100);
+	});
+
 	$('.carousel-services').owlCarousel({
 		loop: true,
 		nav: true,
 		smartspeed: 700,
 		navText: ['<i class="fa fa-angle-double-left"></i>', '<i class="fa fa-angle-double-right"></i>'],
 		responsiveClass: true,
+		dots: false,
 		responsive: {
 			0: {
 				items:1
@@ -49,6 +57,16 @@ $(function() {
 				ths.find('.carousel-services-image').css('min-height', thsh);
 		});
 	}carouselService();
+
+	$('.carousel-services-composition .h3').each(function() {
+		var ths = $(this);
+		ths.html(ths.html().replace(/(\S+)\s*$/, '<span>$1</span>'));
+	});
+	//Resize window
+	function onResize() {
+			$('.carousel-services-contont').equalHeights();
+	}onResize();
+	window.onresize = function() {onResize()};
 
 });
 
