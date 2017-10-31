c40a4d450d6f27c4c342ff2e958dd9201d40396c
Roman Tsymbal <roman.tsymbal@gmail.com>
2017-10-31 15:43:51 +0100

    selectize plugin config

--- pretty-girl/app/js/common.js
+++ pretty-girl/app/js/common.js
@@ -68,9 +68,26 @@ $(function() {
 		ths.html(ths.html().replace(/^(\S+)/, '<span>$1</span>'));
 	});
 
-	// $('select').selectize({
-	// 	craate: true,
+	$('select').selectize();
 
+
+	//E-mail Ajax Send
+	$("form.callback").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			$(th).find('.success').addClass('active').css('display', 'flex').hide().fadeIn();
+			setTimeout(function() {
+				// Done Functions
+				$(th).find('.success').removeClass('active').fadeOut();
+				th.trigger("reset");
+			}, 3000);
+		});
+		return false;
+	});
 	// });
 	//Resize window
 	function onResize() {
