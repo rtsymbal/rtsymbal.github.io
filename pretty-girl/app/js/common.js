54ca390c3a9f41a3372cb858a3b3219cfb599882
Roman Tsymbal <roman.tsymbal@gmail.com>
2017-11-01 14:59:26 +0100

    partners block coding

--- pretty-girl/app/js/common.js
+++ pretty-girl/app/js/common.js
@@ -46,7 +46,7 @@ $(function() {
 			},
 			1100: {
 				items: 3
-			}
+			},
 		}
 	});
 
@@ -75,8 +75,32 @@ $(function() {
 		items: 1,
 		smartSpeed: 700,
 		nav: false,
-		autoHeight: true,
+		autoHeight: true
 	});
+
+	$('.partners').owlCarousel({
+		loop: true,
+		smartSpeed: 700,
+		dots: false,
+		nav: true,
+		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			768: {
+				items: 2
+			},
+			992: {
+				items: 3
+			},
+			1200: {
+				items: 4
+			}
+		}
+	});
+
 
 	//E-mail Ajax Send
 	$("form.callback").submit(function() { //Change
