7355ebc9183d160799740167bc6877a1d507704d
Roman Tsymbal <roman.tsymbal@gmail.com>
2017-11-15 10:09:42 +0100

    add script

--- motoservice/app/js/common.js
+++ motoservice/app/js/common.js
@@ -1,5 +1,14 @@
 $(function() {
 
-	// Custom JS
+	$(".mc_item_wrap ul").each(function() {
+		$(this).after("<div class='mc_item_wrap_af'></div>");
+	});
+	$(".mc_toggle").click(function() {
+		$("body .mc_wrap .mc_item_wrap > ul, .mc_item_wrap_af").hide();
+		$(".mc_item_wrap").removeClass("active");
+		$(this).parent().parent().addClass("active");
+		$(this).parent().parent().children("ul").show();
+		$(this).parent().parant().children(".mc_item_wrap_af").show(0);
+	});
 
 });
