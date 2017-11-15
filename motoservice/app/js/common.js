feb188e59d5c8fccd70609180c4cd6d72eb52b20
Roman Tsymbal <roman.tsymbal@gmail.com>
2017-11-15 21:41:29 +0100

    sub menu coding

--- motoservice/app/js/common.js
+++ motoservice/app/js/common.js
@@ -1,11 +1,12 @@
 $(function() {
 
-	$(".mc_item_wrap ul").each(function() {
-		$(this).after("<div class='mc_item_wrap_af'></div>");
-	});
-	$(".mc_toggle").click(function() {
+	// $(".mc_item_wrap ul").each(function() {
+	// 	$(this).after("<div class='mc_item_wrap_af'></div>");
+	// });
+
+	$(".arrow-toggle").click(function() {
 		$("body .mc_wrap .mc_item_wrap > ul, .mc_item_wrap_af").hide();
-		$(".mc_item_wrap").removeClass("active");
+		$(".category-items-wrap").removeClass("active");
 		$(this).parent().parent().addClass("active");
 		$(this).parent().parent().children("ul").show();
 		$(this).parent().parant().children(".mc_item_wrap_af").show(0);
