aab767c9a70de1af74faef957df071a9441657d8
Roman Tsymbal <roman.tsymbal@gmail.com>
2017-11-21 20:39:25 +0100

    content category coding

--- motoservice/app/js/common.js
+++ motoservice/app/js/common.js
@@ -4,12 +4,12 @@ $(function() {
 	// 	$(this).after("<div class='mc_item_wrap_af'></div>");
 	// });
 
-	// $(".arrow-toggle").click(function() {
-	// 	$("body .mc_wrap .mc_item_wrap > ul, .mc_item_wrap_af").hide();
+	// $("ul li .arrow-toggle").click(function() {
+	// 	//$(".pull-list").hide();
 	// 	$(".category-items-wrap").removeClass("active");
 	// 	$(this).parent().parent().addClass("active");
 	// 	$(this).parent().parent().children("ul").show();
-	// 	$(this).parent().parant().children(".mc_item_wrap_af").show(0);
+	// 	//$(this).parent().parant().children(".mc_item_wrap_af").show(1000);
 	// });
 
 });
