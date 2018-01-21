2273b867b10d5ee8b2ca5233580347dcbcfa972f
Roman Tsymbal <roman.tsymbal@gmail.com>
2018-01-22 00:12:42 +0200

    buttons animation

--- travel/develop/_scripts/common.js
+++ travel/develop/_scripts/common.js
@@ -1,3 +1,5 @@
+$(function() {
+
 // ---------Put first word in span
 
 $('.rt-text-field H2').each(function() {
@@ -10,4 +12,26 @@ $('.rt-text-field H2').each(function() {
 $('.rt-section-title__title').each(function() {
 	var ths = $(this);
 	ths.html(ths.html().replace(/(\S+)\s*$/, '<span>$1</span>'));
+});
+
+
+
+
+
+$('.rt-button')
+  .on('mouseenter', function(e) {
+		var parentOffset = $(this).offset(),
+    		relX = e.pageX - parentOffset.left,
+    		relY = e.pageY - parentOffset.top;
+		$(this).find('span').css({top:relY, left:relX})
+  })
+  .on('mouseout', function(e) {
+		var parentOffset = $(this).offset(),
+    		relX = e.pageX - parentOffset.left,
+    		relY = e.pageY - parentOffset.top;
+  	$(this).find('span').css({top:relY, left:relX})
+  });
+
+
+
 });
